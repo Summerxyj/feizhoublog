f2e2fc4a86b5ae26de3c7c7af93889cd4ac96221
xyj <xyj@rimideiMac-3.local>
2018-11-09 16:22:13 +0800

    第一次测试

---
+++ feizhou-xyj/js/feizhou.js
@@ -0,0 +1,20 @@
+function foo1()
+{
+	var a = document.getElementsByClassName("div-search1")[0];
+	var b = document.getElementsByClassName("div-search2")[0];
+	a.style.display = "none";
+	b.style.display = "block";
+}
+function foo2()
+{
+	var a = document.getElementsByClassName("div-search1")[0];
+	var b = document.getElementsByClassName("div-search2")[0];
+	a.style.display = "block";
+	b.style.display = "none";
+}
+
+function foo3()
+{
+	var a = document.getElementById("rp-1")
+	a.style.title = "腾讯QQ"
+}
